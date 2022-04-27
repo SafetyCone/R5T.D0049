19001c75001dea913beaa4945df6ae55184c0768
davidcoats <david@getsafetycone.com>
2022-04-27 15:27:39 -0700

    Interim changes.

--- source/R5T.D0049.Base/Code/Services/Definitions/IExceptionSink.cs
+++ source/R5T.D0049.Base/Code/Services/Definitions/IExceptionSink.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Threading.Tasks;
 
+using R5T.T0064;
+
 
 namespace R5T.D0049
 {
-    public interface IExceptionSink
+    [ServiceDefinitionMarker]
+    public interface IExceptionSink : IServiceDefinition
     {
         Task Consume(Exception exception);
     }
